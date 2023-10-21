2d7dce89c1519b44121907f523e74e8c1f4e17b2
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2023-10-21 23:25:55 +0700

    update

---
+++ src/shared/ListOfContacts.js
@@ -0,0 +1,10 @@
+export const Contacts = [
+    {
+      id: 1,
+      name: 'Vinh Quang',
+      address: 'USA',
+      img: 'assets/images/StarWars1.jpg',
+      info: 'Chiến tranh giữa các vì sao: Tập I – Hiểm họa bóng ma là một bộ phim sử thi không gian của Mỹ năm 1999 được biên kịch và đạo diễn bởi George Lucas, sản xuất bởi Lucasfilm và phân phối bởi 20th Century Fox.'
+    }
+];
+export default Contacts;
